d2d239a1f53460a15e30f25974452f4fdeb3b974
T H <>
2018-09-17 21:11:59 +0200

    compile time aspect version
    no compile errors

--- mvnaspect/src/main/java/com/mycompany/app/JapaneseMannersAspect.java
+++ mvnaspect/src/main/java/com/mycompany/app/JapaneseMannersAspect.java
@@ -1,14 +1,28 @@
 package com.mycompany.app;
 
-// JapaneseMannersAspect.java
-public aspect JapaneseMannersAspect {
-    pointcut callSayMessageToPerson(String person) 
-	: call(* HelloWorld.sayToPerson(String, String))
-	       && args(*, person);
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
-    void around(String person) 
-	: callSayMessageToPerson(person) {
+// JapaneseMannersAspect.java
+@Aspect
+public class JapaneseMannersAspect {
+
+	@Pointcut("call (* HelloWorld.sayToPerson(String, String)) && args(msg, person)")
+	public void callSayMessageToPerson(String msg, String person) {
+
+	}
+
+	@Around("callSayMessageToPerson(msg, person)")
+	public void aroundAdvice(ProceedingJoinPoint joinPoint, String msg, String person) {
 		System.out.println("Aspect around:");
-	proceed(person + "-san");
-    }
+		String personSan = person + "-san";
+		try {
+			joinPoint.proceed(new Object[] { msg, personSan });
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- mvnaspect/src/main/java/com/mycompany/app/MannersAspect.java
+++ mvnaspect/src/main/java/com/mycompany/app/MannersAspect.java
@@ -1,14 +1,25 @@
 package com.mycompany.app;
 
-// MannersAspect.java
-public aspect MannersAspect {
-    pointcut callSayMessage() : call(public static void HelloWorld.say*(..));
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
-    before() : callSayMessage() {
+// MannersAspect.java
+@Aspect
+public class MannersAspect {
+	@Pointcut("call(public static void HelloWorld.say*(..))")
+    public void  callSayMessage() {
+		
+	}
+	
+    @Before("callSayMessage()")
+    public void sayMessageBeforeAdvice(){
 	System.out.println("Aspect before: Good day!");
     }
 
-    after() : callSayMessage() {
+    @After("callSayMessage()")
+    public void sayMessageAfterAdvice(){
 	System.out.println("Aspect after: Thank you!");
     }
 }
